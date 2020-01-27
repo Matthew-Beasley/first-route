6ee177495e843c46074587121e98db03619f6908
conbec <56614504+conbec@users.noreply.github.com>
2020-01-27 14:29:54 -0800

    Incomplete, still following Prof

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API = 'https://acme-users-api-rev.herokuapp.com';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [companies, setCompanies] = useState([]);
+  const [view, setView] = useState('products');
+
+
+  useEffect(() => {
+    axios.get(`${API}/api/companies`)
+      .then(response => setCompanies(response.data));
+  }, []);
+
+  useEffect(() => {
+    axios.get(`${API}/api/products`)
+    .then(response => setProducts(response.data))
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+
+      </nav>
+      <main>
+        { view === 'products' && <Products products={products} /> }
+        { view === 'companies' && <Companies companies={companies}/> }
+
+      </main>
     </div>
   );
 }
